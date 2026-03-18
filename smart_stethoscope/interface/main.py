c24f0ebb3a3b0bf57d20b332c512fb3aa20865f3
Mia Meyer <mia.meyer@outlook.com>
2026-03-18 16:13:37 +0100

    add params

--- smart_stethoscope/interface/main.py
+++ smart_stethoscope/interface/main.py
@@ -5,13 +5,14 @@ from smart_stethoscope.ml_logic.preprocessing import preprocess_tabular_data
 def preprocessing():
     data = load_data()
     X_train, X_test, y_train, y_test, train_pids, test_pids = preprocess_tabular_data(
-        data,
-        pipeline_save_path='models/post_split_pipeline.pkl'
+        data, pipeline_save_path="models/post_split_pipeline.pkl"
     )
+    return X_train, X_test, y_train, y_test, train_pids, test_pids
 
 
 def train():
     pass
 
-if __name__ == '__main__':
-    preprocessing() #safety recommendation
+
+if __name__ == "__main__":
+    preprocessing()  # safety recommendation
