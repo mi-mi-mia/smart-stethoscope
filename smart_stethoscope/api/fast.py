662ea611fe06dffd8632b1d798c0f0dd75255e64
Linus Schroeter <linusschroeter@gmail.com>
2026-03-25 12:00:08 +0100

    worked on fast.py

--- smart_stethoscope/api/fast.py
+++ smart_stethoscope/api/fast.py
@@ -4,20 +4,24 @@ import numpy as np
 import pandas as pd
 import librosa
 from fastapi import FastAPI, UploadFile, File
+import pickle
 from tensorflow import keras
 from smart_stethoscope.ml_logic.audio_preprocessing import (
-    preprocess_audio,
+    preprocess_audio, # need to update function name
     build_mel_spectrogram_dataset
 )
 from smart_stethoscope.params import TARGET_SAMPLING_RATE
 
-# Load once at startup
-MODEL_PATH = os.getenv("MODEL_PATH", "models/best_cnn_model.keras")
-model = keras.models.load_model(MODEL_PATH)
+# Load once at startup.
+# this still loads locally? needs to be changed to gcloud?
+MODEL_PATH = os.getenv("MODEL_PATH", "models/best_cnn_model.keras") #needs to be updated?
+xgb_model = pickle.load(open("models/xgb_model.pkl","rb")) #will this be a pickkle file?
+cnn_model = keras.models.load_model(MODEL_PATH)
+
 
 DISEASE_MAPPING_INV = {
     0: 'Healthy', 1: 'COPD', 2: 'URTI',
-    3: 'Bronchiectasis', 4: 'Pneumonia', 5: 'Bronchiolitis'
+    3: 'Bronchiectasis', 4: 'Pneumonia'#, 5: 'Bronchiolitis' got dropped
 }
 
 app = FastAPI()
@@ -29,30 +33,34 @@ def index():
 @app.post("/predict")
 async def predict_audio(
     audio_file: UploadFile = File(...),
-    annotation_file: UploadFile = File(...)
+    #annotation_file: UploadFile = File(...)
+    start: float,
+    end: float,
 ):
     # 1. Read both files into memory as bytes
     audio_bytes = await audio_file.read()
-    annotation_bytes = await annotation_file.read()
+    #annotation_bytes = await annotation_file.read()
 
     # 2. Load audio into numpy array — no disk write needed
     audio, sr = librosa.load(io.BytesIO(audio_bytes), sr=None)
 
     # 3. Parse annotation .txt into a DataFrame
-    annotations = pd.read_csv(
-        io.StringIO(annotation_bytes.decode("utf-8")),
-        sep="\t",
-        names=["start", "end", "crackles", "wheezes"]
-    )
+    #annotations = pd.read_csv(
+    #    io.StringIO(annotation_bytes.decode("utf-8")),
+    #    sep="\t",
+    #    names=["start", "end", "crackles", "wheezes"]
+    #)
 
-    # 4. Preprocess: resample, slice cycles, pad/trim
-    padded_audios = preprocess_audio(audio, sr, annotations)
+    # 4. Preprocess: resample, slice cycles, trim
+    xgb_df, cnn_df = preprocess_audio(audio, sr, start, end) #need to update function name
 
     # 5. Convert breathing cycles into mel spectrograms for CNN
-    features = build_mel_spectrogram_dataset(padded_audios)
+    #features = build_mel_spectrogram_dataset(padded_audios)
 
     # 6. Predict per cycle — CNN returns probabilities, argmax gives class
-    probabilities = model.predict(features)         # shape: (n_cycles, 6)
+    probabilities_xgb = xgb_model.predict(xgb_df) # shape: (n_cycles, 6)
+    probablities_cnn = cnn_model.predict(cnn_df)
+
     predicted_ints = np.argmax(probabilities, axis=1)  # one per cycle
 
     # 7. Majority vote across cycles → single prediction per recording
