b51d6942a22c43f1336b06b274e89cab4025999e
Linus Schroeter <linusschroeter@gmail.com>
2026-03-25 12:36:32 +0100

    worked on fast.py

--- smart_stethoscope/api/fast.py
+++ smart_stethoscope/api/fast.py
@@ -18,6 +18,25 @@ MODEL_PATH = os.getenv("MODEL_PATH", "models/best_cnn_model.keras") #needs to be
 xgb_model = pickle.load(open("models/xgb_model.pkl","rb")) #will this be a pickkle file?
 cnn_model = keras.models.load_model(MODEL_PATH)
 
+#CODE FROM MLOPS exc for loading model from gcs
+#def load_model():
+#      client = storage.Client()
+#       blobs = list(client.get_bucket(BUCKET_NAME).list_blobs(prefix="model"))
+#
+#       try:
+#           latest_blob = max(blobs, key=lambda x: x.updated)
+#           latest_model_path_to_save = os.path.join(LOCAL_REGISTRY_PATH, latest_blob.name)
+#           latest_blob.download_to_filename(latest_model_path_to_save)
+#
+#           latest_model = keras.models.load_model(latest_model_path_to_save)
+#
+#           print("✅ Latest model downloaded from cloud storage")
+#
+#           return latest_model
+#       except:
+#           print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
+#
+#           return None
 
 DISEASE_MAPPING_INV = {
     0: 'Healthy', 1: 'COPD', 2: 'URTI',
@@ -58,7 +77,7 @@ async def predict_audio(
     #features = build_mel_spectrogram_dataset(padded_audios)
 
     # 6. Predict per cycle — CNN returns probabilities, argmax gives class
-    probabilities_xgb = xgb_model.predict(xgb_df) # shape: (n_cycles, 6)
+    probabilities_xgb = xgb_model.predict_proba(xgb_df) # shape: (n_cycles, 6)
     probablities_cnn = cnn_model.predict(cnn_df)
 
     predicted_ints = np.argmax(probabilities, axis=1)  # one per cycle
