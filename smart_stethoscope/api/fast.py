8a2eb3ae643c986f700e18ea4ca3f9e300b7f412
Mia Meyer <mia.meyer@outlook.com>
2026-03-25 16:43:00 +0100

    small clean up

--- smart_stethoscope/api/fast.py
+++ smart_stethoscope/api/fast.py
@@ -1,17 +1,15 @@
 import os
 import io
-import numpy as np
-import pandas as pd
 import librosa
 from fastapi import FastAPI, UploadFile, File, Form
 from tensorflow import keras
 
 from smart_stethoscope.interface.main import preprocess_for_prediction, predict
-from smart_stethoscope.params import TARGET_SAMPLING_RATE
-from google.cloud import storage
 
 # Load once at startup
-MODEL_PATH = os.getenv("MODEL_PATH", "gs://smart-stethoscope-models/best_cnn_model.keras")
+MODEL_PATH = os.getenv(
+    "MODEL_PATH", "gs://smart-stethoscope-models/best_cnn_model.keras"
+)
 model = keras.models.load_model(MODEL_PATH)
 
 
@@ -25,9 +23,7 @@ def index():
 
 @app.post("/predict")
 async def predict_audio(
-    audio_file: UploadFile = File(...),
-    start: float=Form(...) ,
-    end: float=Form(...)
+    audio_file: UploadFile = File(...), start: float = Form(...), end: float = Form(...)
 ):
     # 1. Read audio file into memory as bytes
     audio_bytes = await audio_file.read()
@@ -41,13 +37,17 @@ async def predict_audio(
     # 4. Predict with hybrid model, output is a dictionary:
     # {"xgb_chunk_proba", "cnn_chunk_proba",
     # "fused_chunk_proba", "final_proba", "final_prediction"}
-    predictions = predict(xgb_model=xgb_model, cnn_model=cnn_model,
-                   xgb_df=xgb_df, cnn_array = cnn_df
-                   )
+    predictions = predict(
+        xgb_model=xgb_model, cnn_model=cnn_model, xgb_df=xgb_df, cnn_array=cnn_df
+    )
 
     # 5. Output
     return {
         # 🎯 Final decision
         "prediction": predictions["final_prediction"],
-        "final_proba": predictions["final_proba"].tolist() if hasattr(predictions["final_proba"], "tolist") else predictions["final_proba"],
+        "final_proba": (
+            predictions["final_proba"].tolist()
+            if hasattr(predictions["final_proba"], "tolist")
+            else predictions["final_proba"]
+        ),
     }
